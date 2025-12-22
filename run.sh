536c4e5b3eaf2e4c6e6d968094b10079b96e38f8
chLi <565353780@qq.com>
2025-12-22 19:10:08 +0800

    remove archive scripts

--- run.sh
+++
@@ -1,5 +0,0 @@
-python demo.py \
-  --method sp_lg \
-  --fig1 $HOME/chLi/Dataset/MM/Match/people_1/input.PNG \
-  --fig2 $HOME/chLi/Dataset/MM/Match/people_1/gen.PNG \
-  --save_dir $HOME/chLi/Dataset/MM/Match/people_1/minima_sp_lg/
