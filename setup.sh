6c9aab1cbc182b9709896f53ab2fcaabf5c5bbfd
chLi <565353780@qq.com>
2025-12-22 19:11:15 +0800

    add pytorch3d support

--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
+cd ..
+git clone https://github.com/facebookresearch/pytorch3d.git
+
 pip3 install torch torchvision torchaudio \
   --index-url https://download.pytorch.org/whl/cu128
 
 pip install numpy opencv_python einops kornia loguru yacs \
   tqdm yq jupyterlab matplotlib pytorch-lightning scipy \
   joblib
+
+cd pytorch3d
+python setup.py install
