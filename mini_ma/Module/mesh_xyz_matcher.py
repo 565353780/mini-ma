70e90c6b46dfb4ca4bd83af37725cb14d5aa325e
chLi <565353780@qq.com>
2026-01-29 23:34:37 +0800

    fix import error

--- mini_ma/Module/mesh_xyz_matcher.py
+++ mini_ma/Module/mesh_xyz_matcher.py
@@ -1,12 +1,12 @@
-from mini_ma.Method.data import toNumpy
 import torch
 import trimesh
 import numpy as np
 from typing import Tuple, Optional
 
-from camera_control.Module.rgbd_camera import RGBDCamera
+from camera_control.Module.camera import Camera
 from camera_control.Module.nvdiffrast_renderer import NVDiffRastRenderer
 
+from mini_ma.Method.data import toNumpy
 from mini_ma.Module.detector import Detector
 from mini_ma.Module.camera_matcher import CameraMatcher
 
@@ -17,7 +17,7 @@ class MeshXYZMatcher(object):
 
     @staticmethod
     def extractMatchedTrianglePoint(
-        rgbd_camera: RGBDCamera,
+        camera: Camera,
         render_dict: dict,
         match_result: dict,
     ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
@@ -26,25 +26,25 @@ class MeshXYZMatcher(object):
             match_result=match_result,
         )
 
-        matched_points, confs, valid_mask = rgbd_camera.queryUVPoints(matched_uv)
+        matched_points, confs, valid_mask = camera.queryUVPoints(matched_uv)
 
         return matched_points, matched_triangle_idxs, confs, valid_mask
 
     @staticmethod
     def queryTrianglePoints(
         mesh: trimesh.Trimesh,
-        rgbd_camera: RGBDCamera,
+        camera: Camera,
         detector: Detector,
     ) -> Tuple[Optional[torch.Tensor], Optional[torch.Tensor], Optional[torch.Tensor], Optional[torch.Tensor]]:
         render_dict = NVDiffRastRenderer.renderNormal(
             mesh,
-            rgbd_camera,
+            camera,
             bg_color=[0, 0, 0],
         )
 
         normal_image_cv = toNumpy(render_dict['normal_camera'] * 255.0, np.uint8)[..., ::-1]
 
-        match_result = detector.detect(rgbd_camera.image_cv, normal_image_cv)
+        match_result = detector.detect(camera.image_cv, normal_image_cv)
 
         if match_result is None:
             print('[ERROR][MeshXYZMatcher::queryTrianglePoints]')
@@ -52,4 +52,4 @@ class MeshXYZMatcher(object):
             return None, None, None, None
 
         return MeshXYZMatcher.extractMatchedTrianglePoint(
-            rgbd_camera, render_dict, match_result)
+            camera, render_dict, match_result)
